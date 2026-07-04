a75c5214199519d795f75a20677639e96e5545d3
Sudhanshu Baberwal <24bcs147@iiitdwd.ac.in>
2026-07-04 11:33:33 +0530

    first commit

---
+++ src/main/java/com/project/Fitness/service/UserService.java
@@ -0,0 +1,51 @@
+package com.project.Fitness.service;
+
+import com.project.Fitness.dto.RegisterRequest;
+import com.project.Fitness.dto.UserResponse;
+import com.project.Fitness.model.User;
+import com.project.Fitness.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserResponse register(RegisterRequest registerRequest) {
+        User user = new User(
+                null,
+                registerRequest.getEmail(),
+                registerRequest.getPassword(),
+                registerRequest.getFirstName(),
+                registerRequest.getLastName(),
+                Instant.parse("2026-07-04T14:14:41.208Z")
+                        .atZone(ZoneOffset.UTC)
+                        .toLocalDateTime(),
+                Instant.parse("2026-07-04T14:14:41.208Z")
+                        .atZone(ZoneOffset.UTC)
+                        .toLocalDateTime(),
+                List.of(),
+                List.of()
+        );
+        User savedUser = userRepository.save(user);
+        return mapToResponse(savedUser);
+    }
+
+    private UserResponse mapToResponse(User user) {
+        UserResponse response = new UserResponse();
+        response.setId(user.getId());
+        response.setEmail(user.getEmail());
+        response.setPassword(user.getPassword());
+        response.setFirstName(user.getFirstName());
+        response.setLastName(user.getLastName());
+        response.setCreatedAt(user.getCreatedAt());
+        response.setUpdatedAt(user.getUpdatedAt());
+        return response;
+    }
+}
